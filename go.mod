fa8dcca2a84d72e288919e7f84aa59f755501bc8
Steve <stryan@saintnet.tech>
2024-08-17 19:20:19 -0400

    init

---
+++ go.mod
@@ -0,0 +1,27 @@
+module git.saintnet.tech/stryan/haldap
+
+go 1.23.0
+
+require (
+	github.com/charmbracelet/log v0.4.0
+	github.com/go-ldap/ldap/v3 v3.4.8
+	github.com/kelseyhightower/envconfig v1.4.0
+)
+
+require (
+	github.com/Azure/go-ntlmssp v0.0.0-20221128193559-754e69321358 // indirect
+	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
+	github.com/charmbracelet/lipgloss v0.10.0 // indirect
+	github.com/go-asn1-ber/asn1-ber v1.5.5 // indirect
+	github.com/go-logfmt/logfmt v0.6.0 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mattn/go-isatty v0.0.18 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/muesli/reflow v0.3.0 // indirect
+	github.com/muesli/termenv v0.15.2 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
+	golang.org/x/crypto v0.21.0 // indirect
+	golang.org/x/exp v0.0.0-20231006140011-7918f672742d // indirect
+	golang.org/x/sys v0.18.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"regexp"
+
+	"github.com/charmbracelet/log"
+	ldap "github.com/go-ldap/ldap/v3"
+	"github.com/kelseyhightower/envconfig"
+)
+
+type User struct {
+	Name      string
+	Group     string
+	LocalOnly bool
+}
+
+type Config struct {
+	URL      string `envconfig:"HALDAP_URL"`
+	BaseDN   string `envconfig:"HALDAP_BASEDN"`
+	UserAttr string `envconfig:"HALDAP_USERATTR"`
+}
+
+func (u *User) String() string {
+	return fmt.Sprintf("name = %v\ngroup = %v\nlocal_only = %v", u.Name, u.Group, u.LocalOnly)
+}
+
+func main() {
+	logger := log.New(os.Stderr)
+	var c Config
+	err := envconfig.Process("haldap", &c)
+	if err != nil {
+		logger.Fatal(err)
+	}
+	uname, ok := os.LookupEnv("username")
+	if !ok {
+		logger.Warn("couldn't get username")
+		return
+	}
+	pwd, ok := os.LookupEnv("password")
+	if !ok {
+		logger.Warn("couldn't get password")
+		return
+	}
+	userdn := fmt.Sprintf("%v=%v,%v", c.UserAttr, uname, c.BaseDN)
+
+	l, err := ldap.DialURL(c.URL)
+	if err != nil {
+		logger.Fatal(err)
+	}
+	defer func() { _ = l.Close() }()
+	err = l.Bind(userdn, pwd)
+	if err != nil {
+		logger.Fatal(err)
+	}
+	logger.Info("bind successful")
+	u := &User{}
+	result, err := l.Search(ldap.NewSearchRequest(
+		userdn,
+		ldap.ScopeWholeSubtree,
+		ldap.NeverDerefAliases,
+		0,
+		0,
+		false,
+		fmt.Sprintf("(&(objectClass=organizationalPerson)(%s=%s))", c.UserAttr, uname),
+		[]string{"displayName", "memberOf"},
+		nil,
+	))
+	if err != nil {
+		logger.Fatal(err)
+	}
+	if len(result.Entries) > 1 {
+		logger.Fatal("expected one result got many")
+	}
+	entry := result.Entries[0]
+	groups := entry.GetAttributeValues("memberOf")
+	re := regexp.MustCompile(`cn=([0-9-a-zA-Z]+),`)
+	for _, group := range groups {
+		g := re.FindStringSubmatch(group)[1]
+		if g == "hausers" {
+			u.Group = "system-users"
+		} else if g == "haadmins" {
+			u.Group = "system-admin"
+		}
+	}
+	if u.Group == "" {
+		logger.Info("user not enabled")
+		return
+	}
+	u.Name = entry.GetAttributeValue("displayName")
+	u.LocalOnly = false
+	fmt.Printf("%v\n", u)
+}
